c39a9a628004c9836b9ef010cb1d7b96752b1443
avinash101999 <82923690+avinash101999@users.noreply.github.com>
2025-02-24 15:08:26 +0530

    Create Hero.js

---
+++ Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Get Groceries in 10 Minutes!</h1>
+    </section>
+  );
+};
+
+export default Hero;
