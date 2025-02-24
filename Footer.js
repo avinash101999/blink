0cbe62b49f67d04054400dfeb8df4f6ec94bdf1b
avinash101999 <82923690+avinash101999@users.noreply.github.com>
2025-02-24 15:10:47 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 QuickMart. All rights reserved.</p>
+      <p>
+        <a href="#">About</a> | <a href="#">Contact</a> | <a href="#">Terms</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
